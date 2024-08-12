19046c53a91c482a7b36cc869b4e6ed4c04732f3
Nattanon Vongnongvang <icenshy@gmail.com>
2024-08-12 23:09:08 +0700

    start make speedtest-lab.sh

---
+++ speedtest-lab.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+#speedtest-cli --secure --csv-header > speedtest_14.csv
+
+#dont space !!!!!!!
+servers_id="$(speedtest-cli --list --secure | grep -Eo '[0-9]+)' | sed 's/)//')"
+
+#Sdata="$(speedtest-cli --list | grep -Eo '^.*[(]'"
+
+for id in $servers_id; do
+    echo "testing speed at server $id..."
+    speedtest-cli --secure --server $id --csv >> speedtest_14.csv
+    echo "server id $id test complete. you can see test data at speedtest_14.csv"
+done
+echo ""
+echo ""
+
+cat speedtest_14.csv
