560320c15636cb45006c81920445914c43a2248b
starnearmeinroad <gg7075495@gmail.com>
2024-08-18 23:53:21 +0700

    add sleep 60 in while loop

--- speedtest-lab.sh
+++ speedtest-lab.sh
@@ -6,10 +6,10 @@ speedtest-cli --secure --csv-header > speedtest_14.csv
 #dont space !!!!!!!
 servers_id="$(speedtest-cli --list --secure | grep -Eo '[0-9]+)' | sed 's/)//')"
 
-#eieissssss
+#eieissssssss
 
 while true; do
-    
+    sleep 60
     for id in $servers_id; do
     echo "testing speed at server $id..."
     speedtest-cli --secure --server $id --csv >> speedtest_14.csv
